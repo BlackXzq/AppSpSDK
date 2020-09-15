cab443353817c60b93b57f9105339752e9c87b54
xuzhanqiang <xuzhanqiang@anji-allways.com>
2020-09-15 14:32:33 +0800

    Update

--- Framework/AppSpSDK.framework/Headers/AppSpSDK-Swift.h
+++ Framework/AppSpSDK.framework/Headers/AppSpSDK-Swift.h
@@ -203,8 +203,28 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #endif
 
 
+SWIFT_CLASS("_TtC8AppSpSDK15AppSpDeviceInfo")
+@interface AppSpDeviceInfo : NSObject
+- (nonnull instancetype)init OBJC_DESIGNATED_INITIALIZER;
+@end
+
+
+
+
+SWIFT_CLASS("_TtC8AppSpSDK11AppSpNotice")
+@interface AppSpNotice : NSObject
+- (nonnull instancetype)init OBJC_DESIGNATED_INITIALIZER;
+@end
+
+
 SWIFT_CLASS("_TtC8AppSpSDK12AppSpService")
 @interface AppSpService : NSObject
+- (nonnull instancetype)init OBJC_DESIGNATED_INITIALIZER;
+@end
+
+
+SWIFT_CLASS("_TtC8AppSpSDK12AppSpVersion")
+@interface AppSpVersion : NSObject
 - (nonnull instancetype)init OBJC_DESIGNATED_INITIALIZER;
 @end
 
@@ -419,8 +439,28 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #endif
 
 
+SWIFT_CLASS("_TtC8AppSpSDK15AppSpDeviceInfo")
+@interface AppSpDeviceInfo : NSObject
+- (nonnull instancetype)init OBJC_DESIGNATED_INITIALIZER;
+@end
+
+
+
+
+SWIFT_CLASS("_TtC8AppSpSDK11AppSpNotice")
+@interface AppSpNotice : NSObject
+- (nonnull instancetype)init OBJC_DESIGNATED_INITIALIZER;
+@end
+
+
 SWIFT_CLASS("_TtC8AppSpSDK12AppSpService")
 @interface AppSpService : NSObject
+- (nonnull instancetype)init OBJC_DESIGNATED_INITIALIZER;
+@end
+
+
+SWIFT_CLASS("_TtC8AppSpSDK12AppSpVersion")
+@interface AppSpVersion : NSObject
 - (nonnull instancetype)init OBJC_DESIGNATED_INITIALIZER;
 @end
 
@@ -635,8 +675,28 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #endif
 
 
+SWIFT_CLASS("_TtC8AppSpSDK15AppSpDeviceInfo")
+@interface AppSpDeviceInfo : NSObject
+- (nonnull instancetype)init OBJC_DESIGNATED_INITIALIZER;
+@end
+
+
+
+
+SWIFT_CLASS("_TtC8AppSpSDK11AppSpNotice")
+@interface AppSpNotice : NSObject
+- (nonnull instancetype)init OBJC_DESIGNATED_INITIALIZER;
+@end
+
+
 SWIFT_CLASS("_TtC8AppSpSDK12AppSpService")
 @interface AppSpService : NSObject
+- (nonnull instancetype)init OBJC_DESIGNATED_INITIALIZER;
+@end
+
+
+SWIFT_CLASS("_TtC8AppSpSDK12AppSpVersion")
+@interface AppSpVersion : NSObject
 - (nonnull instancetype)init OBJC_DESIGNATED_INITIALIZER;
 @end
 
